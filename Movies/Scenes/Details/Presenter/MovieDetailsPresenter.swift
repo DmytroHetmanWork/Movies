1e0e38e9efe71a2a8199b5ee2cce5942f13b883d
DmytroHetmanWork <hetmandmytrowork@gmail.com>
2025-01-26 10:19:23 +0200

    Feature Movie Details
    + added base

---
+++ Movies/Scenes/Details/Presenter/MovieDetailsPresenter.swift
@@ -0,0 +1,20 @@
+//
+//  MovieDetailsPresenter.swift
+//  Movies
+//
+//  Created by Dmytro Hetman on 26.01.2025.
+//
+
+import Foundation
+
+protocol MovieDetailsPresenterProtocol: AnyObject {
+
+    
+    
+}
+
+final class MovieDetailsPresenter: MovieDetailsPresenterProtocol {
+    
+    
+    
+}
---
+++ Movies/Scenes/Details/View/MovieDetailsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MovieDetailsViewController.swift
+//  Movies
+//
+//  Created by Dmytro Hetman on 26.01.2025.
+//
+
+import UIKit
+
+protocol MovieDetailsView: AnyObject {
+    
+}
+
+final class MovieDetailsViewController: UIViewController, MovieDetailsView {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+
+}
